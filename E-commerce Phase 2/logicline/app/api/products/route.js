80542e824b8888ee224c2c020e704743c61b8381
nashwa-na1901556 <na1901556@qu.edu.qa>
2024-05-02 20:41:12 +0300

    .

--- E-commerce Phase 2/logicline/app/api/products/route.js
+++ E-commerce Phase 2/logicline/app/api/products/route.js
@@ -1,7 +1,9 @@
 import ProductsRepo from "@/app/repo/products-repo"
 const productsRepo = new ProductsRepo()
 
-export async function GET(request){
-    const products =  await productsRepo.getProducts()
-    return Response.json(products,{ status: 200 })
+ 
+export async function GET(request) {
+  const users = await productsRepo.getProducts();
+  return Response.json(users);
+
 }
--- E-commerce Phase 2/logicline/app/repo/products-repo.js
+++ E-commerce Phase 2/logicline/app/repo/products-repo.js
@@ -1,47 +1,63 @@
-import fs from 'fs-extra'
-import { nanoid } from 'nanoid'
-import path from 'path'
+// import fs from 'fs-extra'
+// import { nanoid } from 'nanoid'
+// import path from 'path'
 
-export default class ProductsRepo {
-    constructor() {
-        this.path = path.join(process.cwd(), 'app/data/products.json')
-        console.log(this.path);
-    }
+// export default class ProductsRepo {
+//     constructor() {
+//         this.path = path.join(process.cwd(), 'app/data/products.json')
+//         console.log(this.path);
+//     }
+
+//     async getProducts() {
+//         const products = await fs.readJSON(this.path)
+//         return products;
+//     }
+
+//     async addProduct(account) {
+//         account.accountNo = nanoid()
+//         const accounts = await this.getAccounts()
+//         accounts.push(account)
+//         await fs.writeJSON(this.path, accounts)
+//         return account
+//     }
+
+//     async getProduct(productId) {
+//         const products = await fs.readJson(this.path)
+//         console.log(products)
+//         const product = products.find(p => p.productId == productId)
+//         return product
+//     }
+
+//     async add(transaction) {
+//         transaction.accountNo = parseInt(transaction.accountNo.toString());
+//         transaction.amount = parseInt(transaction.amount.toString());
+//         try {
+//             const accounts = await this.getAccounts();
+//             const account = accounts.find(account => account.accountNo == transaction.accountNo);
+//             if (transaction.transType == 'Deposit') {
+//                 account.deposit(transaction.amount);
+//             } else {
+//                 account.withdraw(transaction.amount);
+//             }
+//             return await fs.writeJson(filePath, accounts)
+//         } catch (err) {
+//             throw err;
+//         }
+//     }
+// }
+
+
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+class ProductsRepo {
 
     async getProducts() {
-        const products = await fs.readJSON(this.path)
-        return products;
-    }
-
-    async addProduct(account) {
-        account.accountNo = nanoid()
-        const accounts = await this.getAccounts()
-        accounts.push(account)
-        await fs.writeJSON(this.path, accounts)
-        return account
-    }
-
-    async getProduct(productId) {
-        const products = await fs.readJson(this.path)
-        console.log(products)
-        const product = products.find(p => p.productId == productId)
-        return product
-    }
-
-    async add(transaction) {
-        transaction.accountNo = parseInt(transaction.accountNo.toString());
-        transaction.amount = parseInt(transaction.amount.toString());
         try {
-            const accounts = await this.getAccounts();
-            const account = accounts.find(account => account.accountNo == transaction.accountNo);
-            if (transaction.transType == 'Deposit') {
-                account.deposit(transaction.amount);
-            } else {
-                account.withdraw(transaction.amount);
-            }
-            return await fs.writeJson(filePath, accounts)
-        } catch (err) {
-            throw err;
+            return prisma.product.findMany(); //prisma.seller.findMany() //get 2 tables 
+
+        } catch (error) {
+            return { error: error.message }
         }
     }
 }
