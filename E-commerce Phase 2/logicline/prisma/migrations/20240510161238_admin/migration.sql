33a58130488ee2db17f3dfbdc87549e8624ee713
nashwa-na1901556 <na1901556@qu.edu.qa>
2024-05-10 19:16:23 +0300

    adding admin table to the database

---
+++ E-commerce Phase 2/logicline/prisma/migrations/20240510161238_admin/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Admin" (
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" TEXT NOT NULL,
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT
+);
--- E-commerce Phase 2/logicline/prisma/seed.js
+++ E-commerce Phase 2/logicline/prisma/seed.js
@@ -7,6 +7,7 @@ const customersPath = path.join(process.cwd(), 'app/data/customers.json')
 const sellersPath = path.join(process.cwd(), 'app/data/sellers.json')
 const productsPath = path.join(process.cwd(), 'app/data/products.json')
 const historyPath = path.join(process.cwd(), 'app/data/purchaseHistory.json')
+const adminsPath = path.join(process.cwd(), 'app/data/admins.json')
 
 async function main() {
     try {
@@ -14,11 +15,13 @@ async function main() {
         const sellers = await fs.readJSON(sellersPath)
         const products = await fs.readJSON(productsPath)
         const history = await fs.readJSON(historyPath)
+        const admins = await fs.readJSON(adminsPath)
     
         for (const c of customers) await prisma.customer.create({ data: c })
         for (const s of sellers) await prisma.seller.create({ data: s })
         for (const p of products) await prisma.product.create({ data: p })
         for (const h of history) await prisma.purchase.create({ data: h })
+        for (const a of admins) await prisma.admin.create({ data: a })
 
     } catch (error) {
         console.log(error);
